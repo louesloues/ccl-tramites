b8a03367ecc53cd8fd9445bd8885ec6be1da52b0
Jose Luis Carreras <joseluiscarreras@192.168.1.11>
2025-06-29 10:37:26 -0600

    Se agrego compoente Buzon y Online

---
+++ src/app/modules/buzon/buzon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buzon',
+  standalone: true,
+  imports: [],
+  templateUrl: './buzon.component.html',
+  styleUrl: './buzon.component.scss'
+})
+export class BuzonComponent {
+
+}
---
+++ src/app/modules/ratificacion/ratificacion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ratificacion',
+  standalone: true,
+  imports: [],
+  templateUrl: './ratificacion.component.html',
+  styleUrl: './ratificacion.component.scss'
+})
+export class RatificacionComponent {
+
+}
